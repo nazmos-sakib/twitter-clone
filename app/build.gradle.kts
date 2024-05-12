8be205258437e047799e6662223daab1dd293082
naz <naz@sakib>
2024-05-12 14:17:08 +0200

    v1 finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,4 +43,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //parse
+    implementation ("com.github.parse-community.Parse-SDK-Android:parse:4.2.0")
 }
--- app/src/main/java/com/example/twitter_clone/MainActivity.java
+++ app/src/main/java/com/example/twitter_clone/MainActivity.java
@@ -2,13 +2,70 @@ package com.example.twitter_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.twitter_clone.databinding.ActivityMainBinding;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "MainActivity";
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(View->{
+            ParseUser.logInInBackground(binding.evUserName.getText().toString(), binding.evPasswords.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (e==null){
+                        Log.i("Login", ": Success");
+                        redirectUser();
+                    } else {
+                        ParseUser newUser = new ParseUser();
+                        newUser.setUsername(binding.evUserName.getText().toString());
+                        newUser.setPassword(binding.evPasswords.getText().toString());
+                        //newUser.put("isFollowing", Pars);
+
+                        newUser.signUpInBackground(exception->{
+                            if (exception==null){
+                                Log.i("Signup", ": success");
+                                redirectUser();
+                            } else {
+                                Toast.makeText(getApplicationContext(),exception.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            });
+        });
+
+
+    }
+
+    void redirectUser(){
+        if (ParseUser.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(), UserActivity.class));
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
